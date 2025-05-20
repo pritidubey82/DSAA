d2bd19e707177fbde533fd16c0c22baab68a87c8
Priti Dubey <144259538+pritidubey82@users.noreply.github.com>
2025-05-20 09:43:20 +0530

    Time: 0 ms (100%), Space: 9 MB (54.96%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count = 0;
+        string ans = "";
+        for (char c : s) {
+            if (c == '(') {
+                if (count > 0) ans += c;
+                count++;
+            } else { // c == ')'
+                count--;
+                if (count > 0) ans += c;
+            }
+        }
+        return ans;
+    }
+};
